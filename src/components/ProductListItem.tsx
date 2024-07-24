d80aeaf34647b65ee948e89c5975156e2a1c244c
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-07-24 21:40:02 +0530

    adding paths safty

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,18 @@
 import { Image, StyleSheet } from 'react-native';
 import Colors from '@/src/constants/Colors';
-import products from '@/assets/data/products';
+import { Product } from '../types';
 import { Text, View,  } from '@/src/components/Themed';
 
-const ProductListItem = ({ product }) => {
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
     return (
       <View style={styles.container}>
-        <Image source={{ uri: product.image }} style={styles.image}/>
+        <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image}/>
         <Text style={styles.title}>{product.name}</Text>
         <Text style={styles.price}>{product.price}</Text>
       </View>
