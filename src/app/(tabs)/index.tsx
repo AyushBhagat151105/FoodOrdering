372af0481a77e9c9c32561b496c2ea26165a6b9a
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-07-24 21:44:46 +0530

    adding FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
 import products from '@/assets/data/products';
 import ProductListItem from '@/src/components/ProductListItem';
-import {  View,  } from '@/src/components/Themed';
+import {  View, } from '@/src/components/Themed';
+import { FlatList } from 'react-native';
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]} />
+      {/* <ProductListItem product={products[0]} />
       <ProductListItem product={products[1]}/>
-      <ProductListItem product={products[3]}/>
+      <ProductListItem product={products[3]}/> */}
+      <FlatList 
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} /> }
+      />
     </View>
   );
 }
