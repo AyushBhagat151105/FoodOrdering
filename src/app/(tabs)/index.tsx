932ac363b8787fcc9a9533647db897deb83d3654
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-07-24 21:26:01 +0530

    creating fitst UI custome components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,13 @@
-import { StyleSheet } from 'react-native';
+import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
+import {  View,  } from '@/src/components/Themed';
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Yoo I am Ayush</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]}/>
+      <ProductListItem product={products[3]}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
